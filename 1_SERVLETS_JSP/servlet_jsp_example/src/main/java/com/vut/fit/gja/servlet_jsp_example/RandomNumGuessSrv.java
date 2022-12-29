0357efd47d8ed4aa35eee4d5ae297e44ed272c6b
b-vit <46031140+bartak-v@users.noreply.github.com>
2022-12-29 21:41:58 +0100

    Populate methods, WIP

--- 1_SERVLETS_JSP/servlet_jsp_example/src/main/java/com/vut/fit/gja/servlet_jsp_example/RandomNumGuessSrv.java
+++ 1_SERVLETS_JSP/servlet_jsp_example/src/main/java/com/vut/fit/gja/servlet_jsp_example/RandomNumGuessSrv.java
@@ -4,29 +4,70 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.Random;
 
 /**
- * An example of HTTP Servlet using Jakarta EE 9.1 API
- * It is an easy random number guesser game using Cookies 
- * for session management.
+ * An example of HTTP Servlet using Jakarta EE 10 API. This is an easy random
+ * number guesser game, using Cookies for simple session management. The purpose
+ * of this example is to showcase a servlet above "Hello World" that utilizes
+ * simple session management through cookies.
+ *
  * @author xbarta47
  */
 public class RandomNumGuessSrv extends HttpServlet {
-    
+
     // Specify explicit serialVersionUID for correct serialization. 
     private static final long serialVersionUID = 42L;
 
+    // Number guessed by user, number of guesses, random number
+    private int numGuessed, numGuesses, secretNumber, score, highestScore;
+    private Random random; // Instance of the Random class    
+    private String username; // User's score and name
+
+    /**
+     * Default no-arg constructor.
+     */
     public RandomNumGuessSrv() {
         super();
     }
 
     /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
+     * Override the parent's init() method. Use it for initializing of variables
+     * needed for the game.
+     *
+     * @throws ServletException if a servlet-specific error occurs
+     */
+    @Override
+    public void init() throws ServletException {
+        // Generate a random number between 1 and 10
+        random = new Random();
+        secretNumber = random.nextInt(10) + 1;
+        // Keep track of the number of guesses the user has made
+        numGuesses = 0;
+        numGuessed = 100;
+        username = "";
+        score = 0;
+        highestScore = 0;
+    }
+
+    /**
+     * Override the parent's destroy() method. Shown here only for demonstration
+     * purposes. Perform cleanup tasks - we don't have to here, since Java has
+     * garbage collection.
+     */
+    @Override
+    public void destroy() {
+    }
+
+    /**
+     * Processes requests for HTTP <code>GET</code>, <code>POST</code>,
+     * <code>PUT</code> and <code>DELETE</code> methods.
      *
      * @param request servlet request
      * @param response servlet response
@@ -35,24 +76,121 @@ public class RandomNumGuessSrv extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
+        String uname = request.getParameter("uname");
+        if (uname != null && !uname.isEmpty()) {
+            response.addCookie(new Cookie("uname", uname));
+            username = uname;
+        } else {
+
+            Cookie cookies[] = request.getCookies();
+            for (Cookie cookie : cookies) {
+                switch (cookie.getName()) {
+                    case "uname":
+                        username = cookie.getValue();
+                        break;
+                    case "uscore":
+                        score = Integer.parseInt(cookie.getValue());
+                        break;
+                }
+            }
+        }
+
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet ExampleServlet</title>");
+            out.println("<link href="
+                    + "\"https://cdn.jsdelivr.net/npm/bootstrap@5.0.2"
+                    + "/dist/css/bootstrap.min.css\" rel=\"stylesheet\" "
+                    + "integrity=\"sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQ"
+                    + "TwFspd3yD65VohhpuuCOmLASjC\" crossorigin=\"anonymous\">");
+            out.println("<title>Random Number Guesser</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet ExampleServlet at " + request.getContextPath() + "</h1>");
+            out.println("<div class=\"d-flex flex-column justify-content-center\">");
+
+            out.println("<div class=\"card font-weight-bold p-2 mx-auto w-50\"><h1>Random Number Guesser Game</h1>");
+            out.println("<h5>Guess random number from 1 to 10, you win if you guess it correctly "
+                    + "in 3 tries.</h5>");
+
+            out.println("<form action=\"/servlet_jsp_example/ExampleServlet\" method=\"POST\">");
+
+            if (username.equals("")) {
+                out.println("<label for=\"uname\">Username:</label>");
+                out.println("<input value=\"username\"  required=\"true\" type=\"text\" id=\"uname\" name=\"uname\"><br><br>");
+            } else {
+                out.println("<label> Hello " + username + " Your score is: " + score + "</label><br><br>");
+            }
+
+            out.println("<label for=\"unum\">Number guessed:</label>");
+            out.println("<input required=\"true\" type=\"text\" id=\"unum\" name=\"unum\"><br><br>");
+            out.println("<button class=\"btn btn-primary\" type=\"submit\">GUESS</button>");
+            out.println("</form>");
+
+            switch (request.getMethod()) {
+                case "DELETE":
+                    out.println("<h1>Game has been forcefully restarted.</h1>");
+                    out.println("<h1>Secret number was " + secretNumber + "</h1>");
+                    break;
+                case "POST":
+                    runGame(request, response);
+                    break;
+                default:
+                    break;
+            }
+
+            out.println("<hr class=\"hr\" /><h5 class=\"text-center\">Number of "
+                    + "guesses " + numGuesses + " of 3 </h5><hr class=\"hr\" />");
+            out.println("</div></div>");
             out.println("</body>");
             out.println("</html>");
         }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
-     * Handles the HTTP <code>GET</code> method.
+     * Handles the game logic.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void runGame(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        numGuessed = Integer.parseInt(request.getParameter("unum"));
+        PrintWriter writer = response.getWriter();
+        // Increment the number of guesses
+        numGuesses++;
+
+        if (numGuesses > 3) {
+            writer.println("You ran out of guesses. The game "
+                    + "has been restarted.");
+            init();
+            // Restart the score.
+            response.addCookie(new Cookie("uscore", Integer.toString(score)));
+            return;
+        }
+
+        // Check if the guess is correct
+        if (numGuessed == secretNumber) {
+            writer.println("Congratulations! You guessed "
+                    + "the secret number " + secretNumber + " in " + numGuesses + " guesses.");
+            writer.println("Restarting the game.");
+            score += 1;
+            response.addCookie(new Cookie("uscore", Integer.toString(score)));
+            init();
+        } else if (numGuessed < secretNumber) {
+            writer.println("Your guess " + numGuessed + " is too low. Try again.");
+        } else {
+            writer.println("Your guess  " + numGuessed + " is too high. Try again.");
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>GET</code> method. Draw the "index" webpage of the
+     * game.
      *
      * @param request servlet request
      * @param response servlet response
@@ -66,7 +204,8 @@ public class RandomNumGuessSrv extends HttpServlet {
     }
 
     /**
-     * Handles the HTTP <code>POST</code> method.
+     * Handles the HTTP <code>POST</code> method. Handle the input from user
+     * based on the cookies and his inputs.
      *
      * @param request servlet request
      * @param response servlet response
@@ -80,6 +219,37 @@ public class RandomNumGuessSrv extends HttpServlet {
     }
 
     /**
+     * Handles the HTTP <code>PUT</code> method. Handle the updated input from
+     * user.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPut(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>DELETE</code> method. Clear cookies and restart
+     * the game to the original state.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+        init();
+    }
+
+    /**
      * Returns a short description of the servlet.
      *
      * @return a String containing servlet description
@@ -88,5 +258,4 @@ public class RandomNumGuessSrv extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }
-
 }
--- README.md
+++ README.md
@@ -2,22 +2,21 @@
 A Java examples project for GJA class at BUT FIT
 
 
-# 1 - Servlets + JSP example program
+# 1 - Servlets + JSP example programs
 
 ### Prerequisites
- The project was developed in [Apache Netbeans 16](https://netbeans.apache.org/download/index.html) with [Eclipse Temurin JDK 17](https://adoptium.net/temurin/releases/?version=17) and [Eclipse Glassfish 7](https://projects.eclipse.org/projects/ee4j.glassfish/releases/7.0.0) (Jakarta EE 9.1 API). Other prerequisite is that the user is already familiar with deploying Java Web Applications onto the server. If not, user can read 
- the documentation of provided tools - as that is out of scope of this project.
+ The project was developed in [Apache Netbeans 16](https://netbeans.apache.org/download/index.html) with [Eclipse Temurin JDK 17](https://adoptium.net/temurin/releases/?version=17) and [Eclipse Glassfish 7](https://projects.eclipse.org/projects/ee4j.glassfish/releases/7.0.0) (Jakarta EE 10 API). Other prerequisite is that the user is already familiar with deploying Java Web Applications onto the server. If not, user can read 
+ the documentation of the provided tools - as that is out of scope of this project.
 
 ### Installation and run 
  - Install Glassfish 7 (either manually or through Netbeans)
  - Run Glassfish (either manually or through Netbeans) 
  - Deploy the compiled .war file onto the Glassfish server (defaultly through [http://localhost:4848](http://localhost:4848))
- - - You can also build the project yourself through standard practice in Netbeans (or manually using maven.)
+ - - You can also build the project yourself through standard practice in Netbeans (or manually using Maven.)
 
 #### Servlet Example
- - The Servlet example is a Random Number Guessing game. The class implements all HTTP requests and utilizes Cookies.
- - You can test the Example Servlet via program like [Postman](https://www.postman.com/) (manipulating raw HTTP requests sent to http://localhost:8080/servlet_jsp_example/ExampleServlet)
- or through a web browser at the same addres.
- - You can test all standard HTTP requests on the 
+ - The Servlet example is a Random Number Guessing game. The class implements HTTP requests and utilizes Cookies for primitive session keeping.
+ - You can test the Example Servlet via  a web browser or "API-testing" program like [Postman](https://www.postman.com/) or CURL (manipulating raw HTTP requests sent to http://localhost:8080/servlet_jsp_example/ExampleServlet) - You need to use something like Postman to test DELETE and PUT methods - as they can't be called from HTML page.
+ - You can test GET, POST, PUT, DELETE  HTTP requests on the Servlet. 
 
 #### JSP Example
