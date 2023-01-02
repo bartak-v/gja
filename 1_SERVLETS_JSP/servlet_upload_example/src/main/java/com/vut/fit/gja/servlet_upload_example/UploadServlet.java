95f2c28811a84e59fa578d071b3eb52eb1a8b2a4
b-vit <46031140+bartak-v@users.noreply.github.com>
2023-01-02 20:35:06 +0100

    Details

--- 1_SERVLETS_JSP/servlet_upload_example/src/main/java/com/vut/fit/gja/servlet_upload_example/UploadServlet.java
+++ 1_SERVLETS_JSP/servlet_upload_example/src/main/java/com/vut/fit/gja/servlet_upload_example/UploadServlet.java
@@ -50,9 +50,13 @@ public class UploadServlet extends HttpServlet {
             + "        <link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css\" rel=\"stylesheet\" \n"
             + "              integrity=\"sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD\" crossorigin=\"anonymous\">\n"
             + "    </head>\n"
-            + "    <body>";
-    private static final String HTML_FOOTER = "</body>\n"
-            + "</html>";
+            + "    <body>"
+            + "<div class=\"card w-50 mx-auto my-5 text-center\">\n"
+            + "        <div class=\"card-header text-info\">\n"
+            + "            GJA - Jakarta EE 10 Upload Servlet Example\n"
+            + "        </div>\n"
+            + "        <div class=\"card-body\">\n";
+    private String HTML_FOOTER;
 
     // Root context path of the servlet (index.html)
     private String contextPath;
@@ -74,34 +78,17 @@ public class UploadServlet extends HttpServlet {
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
         contextPath = super.getServletContext().getContextPath();
+
+        HTML_FOOTER = "</div>\n"
+                + "<div class=\"card-footer text-muted\">\n"
+                + "<a href=\"" + contextPath + "\">Go back to upload.</a>\n"
+                + "</div>\n"
+                + "</div>\n"
+                + "</body>\n"
+                + "</html>";
+
         if (!new File(UPLOAD_DIRECTORY).isDirectory()) {
             throw new ServletException(UPLOAD_DIRECTORY + " is not a directory");
-        }
-    }
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet UploadServlet</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<a href=\"" + contextPath + "\">Go back to upload.</a>");
-            out.println("</body>");
-            out.println("</html>");
         }
     }
 
@@ -116,7 +103,12 @@ public class UploadServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println(HTML_HEADER);
+            out.println("<h4 class=\"card-title\"><strong>File Upload Servlet</strong></h4>\n");
+            out.println(HTML_FOOTER);
+        }
     }
 
     /**
@@ -131,52 +123,38 @@ public class UploadServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            // Print the HTML header
+            out.println(HTML_HEADER);
+            // Check if a file was actually uploaded
+            Part filePart = request.getPart("file");
+            if (filePart == null || filePart.getSubmittedFileName().isEmpty()) {
+                out.println("<h1>No file uploaded.</h1>");
+                out.println(HTML_FOOTER);
+                return;
+            }
+            // Get the uploaded filename
+            String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString();
 
-        PrintWriter out = response.getWriter();
-        // Check if a file was actually uploaded
-        Part filePart = request.getPart("file");
-        if (filePart == null || filePart.getSubmittedFileName().isEmpty()) {
-            out.println("<h1>No file uploaded.</h1> <a href=\"" + contextPath + "\">Go back to upload.</a>");
-            return;
+            // Validate the file's extension
+            if (!fileName.endsWith(".jpg") && !fileName.endsWith(".jpeg") && !fileName.endsWith(".png")) {
+                out.println("<h1>Invalid file type - .jpg, .jpeg, and .png are allowed.</h1>");
+                out.println(HTML_FOOTER);
+                return;
+            }
+            // Save the file to the specified location
+            try (InputStream inputStream = filePart.getInputStream()) {
+                Files.copy(inputStream, Paths.get(UPLOAD_DIRECTORY + fileName), StandardCopyOption.REPLACE_EXISTING);
+            } catch (IOException e) {
+                out.println("<h1>Error while uploading the file.</h1>");
+                out.println(HTML_FOOTER);
+            }
+
+            // Print the HTML page
+            out.println("            <h4 class=\"card-title\"><strong>File " + fileName + " uploaded.</strong></h4>\n"
+                    + "            <div class=\"rounded m-3 p-3 border text-danger\"> Your guess 7 is too low. Try again.</div>\n");
+            out.println(HTML_FOOTER);
         }
-        // Get the uploaded filename
-        String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString();
-
-        // Validate the file's extension
-        if (!fileName.endsWith(".jpg") && !fileName.endsWith(".jpeg") && !fileName.endsWith(".png")) {
-            out.println("<h1>Invalid file type - .jpg, .jpeg, and .png are allowed.</h1> <a href=\"" + contextPath + "\">Go back to upload.</a>");
-            return;
-        }
-        // Save the file to the specified location
-        try (InputStream inputStream = filePart.getInputStream()) {
-            Files.copy(inputStream, Paths.get(UPLOAD_DIRECTORY + fileName), StandardCopyOption.REPLACE_EXISTING);
-        } catch (IOException e) {
-            out.println("<h1>Error while uploading the file.</h1> <a href=\"" + contextPath + "\">Go back to upload.</a>");
-        }
-
-        out.println("<h1>"); // TODO remove
-
-        // Print the HTML page
-        out.println(HTML_HEADER);
-        out.println("<div class=\"card w-50 mx-auto my-5 text-center\">\n"
-                + "        <div class=\"card-header text-info\">\n"
-                + "            GJA - Jakarta EE 10 Upload Servlet Example\n"
-                + "        </div>\n"
-                + "        <div class=\"card-body\">\n"
-                + "            <h4 class=\"card-title\"><strong>Random Number Guesser Game</strong></h4>\n"
-                + "            <p class=\"card-text\">File " + fileName + " uploaded.</h1></p>\n"
-                + "            <a href=\"#\" class=\"btn btn-primary\">Guess</a>\n"
-                + "            <div class=\"d-flex mx-auto w-50 my-3 flex-row justify-content-center\">\n"
-                + "                <div class=\"p-2 mx-1 flex-fill rounded border border-secondary\">User: 5 </div>\n"
-                + "                <div class=\"p-2 mx-1 flex-fill rounded border border-secondary\">Score: 5</div>\n"
-                + "                <div class=\"p-2 mx-1 flex-fill rounded border border-secondary\">Record: 5</div>\n"
-                + "            </div>\n"
-                + "            <div class=\"rounded m-3 p-3 border text-danger\"> Your guess 7 is too low. Try again.</div>\n"
-                + "        </div>\n"
-                + "        <div class=\"card-footer text-muted \">\n"
-                + "            Number of guesses <div class=\"text-danger\">3 of 3</div>\n"
-                + "        </div> <a href=\"" + contextPath + "\">Go back to upload.</a>\n"
-                + "    </div>");
     }
 
     /**
