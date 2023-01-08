629d1f29056c0bfe6712ee363505f0c60f222096
b-vit <46031140+bartak-v@users.noreply.github.com>
2023-01-08 13:34:58 +0100

    Accordion done

--- 5_PRIMEFACES/PFAccordion/src/main/java/org/primefaces/showcase/view/panel/TabbedView.java
+++ 5_PRIMEFACES/PFAccordion/src/main/java/org/primefaces/showcase/view/panel/TabbedView.java
@@ -5,11 +5,11 @@ package org.primefaces.showcase.view.panel;
  
 import java.util.ArrayList;
 import java.util.List;
-import javax.annotation.PostConstruct;
-import javax.enterprise.context.RequestScoped;
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
-import javax.inject.Named;
+import jakarta.annotation.PostConstruct;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+import jakarta.inject.Named;
 
 import org.primefaces.event.TabChangeEvent;
 import org.primefaces.event.TabCloseEvent;
